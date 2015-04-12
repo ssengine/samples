21050167aa62c1616a55edfe921f1bc0872270aa
DengYun <tdzl2003@gmail.com>
2015-04-12 21:11:50 +0800

    support camera and onSizeChanged event.

--- simple_project/res/main.lua
+++ simple_project/res/main.lua
@@ -8,17 +8,21 @@ local s2d = require("stage2d")
 local Stage2D = s2d.Stage2D
 
 local stage = Stage2D.new()
+stage.camera:setPosition(0, 0)
 
 function stage:render()
 	r2d:matrixPush()
 	self:transMatrix(rc)
-	r2d:drawImage(texture, -1, 1, 2, -2, 0, 0, 1, 1)
+	r2d:drawImage(texture, -0.5*100, 0.5*100, 0.5*100, -0.5*100, 0, 0, 1, 1)
 	r2d:matrixPop()
 end
 
 display:add(stage)
 
-runtime:setInterval(function()
-	stage.x = stage.x + 0.01
+runtime:setInterval(function(ev)
+	-- stage.x = stage.x + 1
+	stage.camera.y = stage.camera.y - 1
 	stage.rotation = stage.rotation +0.1
-end, 0.2)
+end, 0.05)
+
+print(display:getSize())
