027157fc2b9a9b205c4ab12e4d0e91a94b071b00
DengYun <tdzl2003@gmail.com>
2015-04-12 14:17:15 +0800

    Working on Stage2D&Node2D

--- simple_project/res/main.lua
+++ simple_project/res/main.lua
@@ -4,28 +4,13 @@ local texture = require("resource").get("texture2d", "res:///qrcode.png")
 
 texture:load()
 
-local count = 100
+local s2d = require("stage2d")
+local Stage2D = s2d.Stage2D
 
-runtime.onTick:addListener(function()
+local stage = Stage2D.new()
+
+stage.render = function()
 	render2d.drawImage(texture, -1, 1, 2, -2, 0, 0, 1, 1)
-	for i = 1, count do
-		render2d.drawLine(math.random(), math.random(), math.random(), math.random())
-	end
-end)
+end
 
-log.info("There" ..runtime:getTime())
-local ev = runtime:setTimeout(function()
-	log.info("Here".. runtime:getTime())
-end, 1)
-
-runtime:setInterval(function()
-	log.info("runtime".. runtime:getTime())
-end, 1)
-
-local localAgent = runtime:new()
-
-localAgent:setInterval(function()
-	log.info("localAgent".. localAgent:getTime())
-end, 0.5)
-
-localAgent:setTimeRatio(0.5)
+display:add(stage)
